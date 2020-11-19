46b99808fe4f360dc2486aeb292364d51a0f146a
bibi.star <bianca.nunes.bacharel@gmail.com>
2020-11-19 17:53:53 -0300

    Configuração

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.port || 8080;
-
-const host = process.env.host || '0.0.0.0';
+const port = process.env.PORT || 8050;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port,host,()=>
+server.listen(port,()=>
 {
     console.log('Json Server is running in ',port);
 
